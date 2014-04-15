a1d15cb122a543caa3cc955e65a06e8d70958b85
Marcin Ignac <vorg@Makowka.local>
2014-04-15 23:37:22 +0100

    Initial code ported from pex v2

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports.SolidColor = require('./lib/SolidColor');
+module.exports.ShowNormals = require('./lib/ShowNormals');
+module.exports.Textured = require('./lib/Textured');
+module.exports.TexturedCubeMap = require('./lib/TexturedCubeMap');
+module.exports.TexturedEnvMap = require('./lib/TexturedEnvMap');
+module.exports.SkyBox = require('./lib/SkyBox');
+module.exports.SkyBoxEnvMap = require('./lib/SkyBoxEnvMap');
