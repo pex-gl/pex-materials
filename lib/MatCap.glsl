d7a934c5ddd273a787ea393cd7a3d780bc7fcabf
Marcin Ignac <marcin.ignac@gmail.com>
2014-04-29 00:43:43 +0100

    White space fix

--- lib/MatCap.glsl
+++ lib/MatCap.glsl
@@ -29,9 +29,9 @@ varying vec3 n;
 void main() {
   vec3 r = reflect(e, n);
   float m = 2.0 * sqrt(
-      pow(r.x, 2.0) +
-      pow(r.y, 2.0) +
-      pow(r.z + 1.0, 2.0)
+    pow(r.x, 2.0) +
+    pow(r.y, 2.0) +
+    pow(r.z + 1.0, 2.0)
   );
   vec2 N = r.xy / m + 0.5;
   vec3 base = texture2D( texture, N ).rgb;
