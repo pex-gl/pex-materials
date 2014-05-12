f5ed583841d367452de6e2dc6756c3c871b47db1
Marcin Ignac <marcin.ignac@gmail.com>
2014-05-12 21:54:53 +0100

    Added ShowPosition for debugging

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports.SolidColor = require('./lib/SolidColor');
 module.exports.ShowNormals = require('./lib/ShowNormals');
 module.exports.ShowColors = require('./lib/ShowColors');
+module.exports.ShowPosition = require('./lib/ShowPosition');
 module.exports.Textured = require('./lib/Textured');
 module.exports.TexturedCubeMap = require('./lib/TexturedCubeMap');
 module.exports.TexturedEnvMap = require('./lib/TexturedEnvMap');
