0ddac1ba4c4fb609678943ea2330664c9b61cdaa
Marcin Ignac <marcin.ignac@gmail.com>
2014-08-16 01:16:35 +0100

    New ShowDepth based on stack overflow

--- lib/ShowDepth.glsl
+++ lib/ShowDepth.glsl
@@ -7,14 +7,21 @@ uniform float far;
 uniform vec4 farColor;
 uniform vec4 nearColor;
 attribute vec3 position;
-attribute vec3 normal;
 varying vec4 vColor;
+
+//http://stackoverflow.com/questions/6652253/getting-the-true-z-value-from-the-depth-buffer
 void main() {
   vec4 pos = modelViewMatrix * vec4(position, 1.0);
   gl_Position = projectionMatrix * pos;
-  float depth = (length(pos.xyz) - near)/(far - near);
-  vColor.rgb = vec3(depth);
-  vColor.a = length(pos.xyz);
+
+  float depth = length(pos.xyz);
+
+  float A = -(far + near) / (far - near); //projectionMatrix[2].z;
+  float B = -2 * far * near / (far - near); //projectionMatrix[3].z;
+
+  depth  = 0.5 * (-A * depth + B) / depth + 0.5;
+
+  vColor = vec4(depth);
 }
 
 #endif
@@ -22,11 +29,9 @@ void main() {
 #ifdef FRAG
 
 varying vec4 vColor;
-uniform float far;
 
 void main() {
-  gl_FragColor.rgb = vColor.rgb;
-  gl_FragColor.a = vColor.a;
+  gl_FragColor = vColor;
 }
 
 #endif
