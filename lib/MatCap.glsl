772146ea560d5fbfd1456f19d0557ed5e6e14680
Marcin Ignac <marcin.ignac@gmail.com>
2014-10-13 00:11:46 +0100

    Fixed MatCap on iOS
    pow(x) where x < 0 is undefined by spec

--- lib/MatCap.glsl
+++ lib/MatCap.glsl
@@ -27,12 +27,8 @@ varying vec3 e;
 varying vec3 n;
 
 void main() {
-  vec3 r = reflect(e, n);
-  float m = 2.0 * sqrt(
-    pow(r.x, 2.0) +
-    pow(r.y, 2.0) +
-    pow(r.z + 1.0, 2.0)
-  );
+  vec3 r = (reflect(e, n));
+  float m = 2.0 * sqrt(r.x * r.x + r.y * r.y + (r.z + 1.0) * (r.z + 1.0));
   vec2 N = r.xy / m + 0.5;
   vec3 base = texture2D( texture, N ).rgb;
   gl_FragColor = vec4( base, 1.0 );
